c3959a07bb1f473e775ab8be5b19232d6eb1feb8
destx0 <destisdbest@gmail.com>
2024-08-17 08:31:56 +0530

    removed dashboard

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,7 +1,6 @@
 import Versions from './components/Versions'
 import electronLogo from './assets/electron.svg'
 import LoginPage from './pages/loginPage'
-import Dashboard from './pages/dashboard'
 import TablePage from './pages/tablePage'
 import StudentFormPage from './pages/studentFormPage'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
@@ -12,7 +11,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/table" element={<TablePage />} />
         <Route path="/add-student" element={<StudentFormPage />} />
         <Route path="/edit-student/:id" element={<StudentFormPage />} />
